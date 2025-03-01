3b4641bc9961af45f498e36c896a9cb07731061c
Santii05-C <santi.c.pgm@gmail.com>
2025-03-01 10:27:14 -0300

    last commit

--- app.py
+++ app.py
@@ -5,9 +5,24 @@ import random
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+# python dict. Store connected users. Key is socket id, value is username and avatarUrl 
+users = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# we're listening for the connect event
+@socketio.on("connect")
+def handle_connect():
+    username = f"User_{random.randint(1000,9999)}"
+    gender = random.choice(["girl","boy"])
+    avatar_url = f" https://avatar.iran.liara.run/public/{gender}?username={username}"
+    
+    users[request.sid] = { "username":username,"avatar":avatar_url}
+    
+    emit("user_joined", {"username":username,"avatar":avatar_url},broadcast=True)
+
 
 if __name__ == "__main__":
     socketio.run (app)
